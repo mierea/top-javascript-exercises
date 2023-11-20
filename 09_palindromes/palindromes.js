e652e1009a181178302a78c6cee5488d910ae2ea
mierea <mierea.ciprian@gmail.com>
2023-11-20 17:25:44 +0200

    Foundations - JS - 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ''); 
+    str = str.replace(/ /g, '');
+    return str.toLowerCase() === str.split("").reverse().join("").toLowerCase();
 };
 
 // Do not edit below this line
