5ee2bec0398ec5643389080137ca3fd06f950047
mierea <mierea.ciprian@gmail.com>
2023-11-20 17:50:52 +0200

    Foundations - JS - 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const currentYear = new Date().getFullYear();
+        const ageCurrent = current.yearOfDeath
+          ? current.yearOfDeath - current.yearOfBirth
+          : currentYear - current.yearOfBirth;
+    
+        const ageOldest = oldest.yearOfDeath
+          ? oldest.yearOfDeath - oldest.yearOfBirth
+          : currentYear - oldest.yearOfBirth;
+    
+        return ageCurrent > ageOldest ? current : oldest;
+      });
 };
 
 // Do not edit below this line
