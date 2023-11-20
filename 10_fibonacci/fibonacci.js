062b7472ff05a2747e3060513d8a803613b6544c
mierea <mierea.ciprian@gmail.com>
2023-11-20 17:29:44 +0200

    Foundations - JS - 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(someNumber) {
+    let a=0, b=1, c, i;
+    if(someNumber < 0) return "OOPS";
+    if(someNumber == 0) return a;
+    for(i=2;i<=someNumber;i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
 
+    return b;
 };
 
 // Do not edit below this line
