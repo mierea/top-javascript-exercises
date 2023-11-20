ada17af374371b79a7b262087b65c433ef17bb65
mierea <mierea.ciprian@gmail.com>
2023-11-20 17:18:55 +0200

    Foundations - JS - 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let initialVal = 0;
+	return arr.reduce((acc, curr) => acc + curr, initialVal,);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let initialVal = 1;
+	return arr.reduce((acc, curr) => acc * curr, initialVal,);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a==0 || a==1) return 1;
+  return a * factorial(a-1);
+
 };
 
 // Do not edit below this line
